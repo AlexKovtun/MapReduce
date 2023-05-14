fc6c04c9fbe42729ea10eb71700fe51928cc4639
AlexKovtun <alexkov.kovtun@gmail.com>
2023-05-14 15:34:39 +0300

    initial state of the project

---
+++ JobContext.cpp
@@ -0,0 +1,5 @@
+//
+// Created by alexk on 14/05/2023.
+//
+
+#include "JobContext.h"
---
+++ JobContext.h
@@ -0,0 +1,12 @@
+//
+// Created by alexk on 14/05/2023.
+//
+
+#ifndef _JOBCONTEXT_H_
+#define _JOBCONTEXT_H_
+
+class JobContext {
+
+};
+
+#endif //_JOBCONTEXT_H_
